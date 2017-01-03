79e448f6f68529e909ae1b37f0d425fa52addbba
dhruv3 <dhruv.mnit@gmail.com>
2017-01-03 17:15:21 -0500

    1. P16: Inserting a node in sorted LL 2. P17: Reverse a single LL

---
+++ src/linkedList/P16.java
@@ -0,0 +1,49 @@
+package linkedList;
+
+import java.util.Scanner;
+
+public class P16 {
+
+	public static void main(String[] args) {
+		LinkedList sList = new LinkedList();
+		createList(sList);
+		System.out.println(sList.toString());
+		Scanner scanner  =  new Scanner(System.in);
+		System.out.println("Mention the node you want to insert");
+		int userInput =  Integer.parseInt(scanner.nextLine());
+		listNode temp =  sList.getHead();
+		int pos = 0;
+		boolean insertAtEnd = true;
+		while(temp.getNode() != null){
+			if(temp.getData() > userInput){
+				sList.insert(userInput, pos);
+				System.out.println(sList.toString());
+				return;
+			}
+			temp = temp.getNode();
+			pos++;
+		}
+		//handle the case when user input is greater than highest data
+		if(temp.getData() <= userInput){
+			sList.insertAtEnd(new listNode(userInput));
+		}
+		else{
+			sList.insert(userInput, pos);
+		}
+		System.out.println(sList.toString());
+	}
+
+	private static void createList(LinkedList sList) {
+		sList.insert(1, 0);
+		sList.insert(3, 1);
+		sList.insert(4, 2);
+		sList.insert(7, 3);
+		sList.insert(11, 4);
+		sList.insert(15, 5);
+		sList.insert(18, 6);
+		sList.insert(20, 7);
+		sList.insert(23, 8);
+		sList.insert(30, 9);
+	}
+
+}
---
+++ src/linkedList/P17.java
@@ -0,0 +1,45 @@
+package linkedList;
+
+
+public class P17 {
+
+	public static void main(String[] args) {
+		LinkedList sList = new LinkedList();
+		createLL(sList);
+		System.out.println("Linked list created is :\n" + sList.toString());
+		listNode reversedsListHead = reversedLL(sList);
+		System.out.println("Reversed linked list is : \n" + print(reversedsListHead));
+	}
+	
+	public static String print(listNode head){
+		String result = "";
+		listNode temp  = head;
+		while(temp.getNode() != null){
+			result = result + temp.getData() + ">";
+			temp = temp.getNode();
+		}
+		result = result + temp.getData();
+		return result;
+	}
+
+	private static listNode reversedLL(LinkedList sList) {
+		listNode prev = null, current = sList.getHead();
+		while(current != null){
+			listNode next = current.getNode();
+			current.setNext(prev);
+			prev = current;
+			current = next;
+		}
+		return prev;
+	}
+
+	private static void createLL(LinkedList sList) {
+		sList.insert(1, 0);
+		sList.insert(2, 1);
+		sList.insert(3, 2);
+		sList.insert(4, 3);
+		sList.insert(5, 4);
+		sList.insert(6, 5);
+	}
+
+}
