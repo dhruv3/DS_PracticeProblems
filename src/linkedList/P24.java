b58ebbd159746d153169a293d086ff76e877506a
dhruv3 <dhruv.mnit@gmail.com>
2017-01-06 18:24:16 -0500

    P24: Finding the merging point of 2 LL P28: Finding the middle element in LL

---
+++ src/linkedList/P24.java
@@ -0,0 +1,73 @@
+package linkedList;
+
+
+public class P24 {
+
+	public static void main(String[] args) {
+		LinkedList sList = new LinkedList();
+		LinkedList dList = new LinkedList();
+		createLL(sList, dList);
+		findMergingPoint(sList, dList);
+	}
+	
+	private static void findMergingPoint(LinkedList sList, LinkedList dList) {
+		listNode sTemp = sList.getHead();
+		listNode dTemp = dList.getHead();
+		int sListLen =  sList.listLength();
+		int dListLen =  dList.listLength();
+		int ctr = 0;
+		//add offset to the pointer in the longer list
+		if(sListLen >= dListLen){
+			while(ctr != (sListLen - dListLen)){
+				sTemp = sTemp.getNode();
+				ctr++;
+			}
+		}
+		else{
+			while(ctr != (dListLen - sListLen)){
+				dTemp = dTemp.getNode();
+				ctr++;
+			}
+		}
+		//add offset to the pointer in the longer list
+		
+		//move in both list till we reach the common node
+		while(dTemp != sTemp){
+			sTemp = sTemp.getNode();
+			dTemp = dTemp.getNode();
+		}
+		//move in both list till we reach the common node
+		
+		System.out.println("Merging node is " + sTemp.getData());
+	}
+
+	private static void createLL(LinkedList sList, LinkedList dList) {
+		sList.insert(1, 0);
+		sList.insert(2, 1);
+		sList.insert(3, 2);
+		sList.insert(4, 3);
+		sList.insert(5, 4);
+		sList.insert(6, 5);
+		
+		dList.insert(1, 0);
+		dList.insert(2, 1);
+		dList.insert(3, 2);
+		
+		listNode c1 = new listNode(8);
+		sList.insertAtEnd(c1);
+		dList.insertAtEnd(c1);
+		
+		listNode c2 = new listNode(9);
+		sList.insertAtEnd(c2);
+		dList.insertAtEnd(c2);
+		
+		listNode c3 = new listNode(10);
+		sList.insertAtEnd(c3);
+		dList.insertAtEnd(c3);
+		
+		listNode c4 = new listNode(11);
+		sList.insertAtEnd(c4);
+		dList.insertAtEnd(c4);
+	}
+
+}
---
+++ src/linkedList/P28.java
@@ -0,0 +1,39 @@
+package linkedList;
+
+
+public class P28 {
+
+	public static void main(String[] args) {
+		LinkedList sList = new LinkedList();
+		createLL(sList);
+		findMidNode(sList);
+	}
+	
+	private static void findMidNode(LinkedList sList) {
+		listNode temp = sList.getHead();
+		listNode dTemp = temp;
+		while(dTemp.getNode() != null){
+			temp =  temp.getNode();
+			if(dTemp.getNode().getNode() != null)
+				dTemp =  dTemp.getNode().getNode();
+			else{
+				dTemp =  dTemp.getNode();
+			}
+		}
+		System.out.println("Middle element is " + temp.getData());
+	}
+
+	private static void createLL(LinkedList sList) {
+		sList.insert(1, 0);
+		sList.insert(2, 1);
+		sList.insert(3, 2);
+		sList.insert(4, 3);
+		sList.insert(5, 4);
+		sList.insert(6, 5);
+		sList.insert(7, 6);
+		sList.insert(16, 7);
+		sList.insert(17, 8);
+		sList.insert(18, 9);
+	}
+
+}
