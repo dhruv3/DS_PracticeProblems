9d8959e8ff999b0870f1292208bae1ad243aec1b
dhruv3 <dhruv.mnit@gmail.com>
2017-01-08 17:49:10 -0500

    P57: Rotate LL by 'k' places.

---
+++ src/linkedList/P57.java
@@ -0,0 +1,63 @@
+package linkedList;
+
+import java.util.Scanner;
+
+public class P57 {
+
+	public static void main(String[] args) {
+		LinkedList sList = new LinkedList();
+		createLL(sList);
+		System.out.println("Intial list " + sList.toString());
+		Scanner scanner  =  new Scanner(System.in);
+		System.out.println("How many elements would you like to rotate?");
+		int userInput =  Integer.parseInt(scanner.nextLine());
+		rotateLL(sList, userInput);
+	}
+	
+
+	private static void rotateLL(LinkedList sList, int userInput) {
+		listNode tempStart = sList.getHead();
+		listNode tempEnd = sList.getHead();
+		int counter = userInput; 
+		while(counter > 0){
+			counter--;
+			tempEnd = tempEnd.getNode();
+			if(tempEnd == null){
+				System.out.println("Number entered is too large compared to list. Terminating.");
+				return;
+			}
+		}
+		
+		while(tempEnd.getNode() != null){
+			tempEnd = tempEnd.getNode();
+			tempStart = tempStart.getNode();
+		}
+		
+		listNode newHead = tempStart.getNode();
+		tempEnd.setNext(sList.getHead());
+		tempStart.setNext(null);
+		System.out.println("Final list " + print(newHead));
+	}
+
+	public static String print(listNode head){
+		String result = "";
+		listNode temp  = head;
+		while(temp.getNode() != null){
+			result = result + temp.getData() + ">";
+			temp = temp.getNode();
+		}
+		result = result + temp.getData();
+		return result;
+	}
+	
+	private static void createLL(LinkedList sList) {
+		sList.insert(1, 0);
+		sList.insert(2, 1);
+		sList.insert(3, 2);
+		sList.insert(4, 3);
+		sList.insert(5, 4);
+		sList.insert(6, 5);
+		sList.insert(7, 6);
+	}
+
+}
