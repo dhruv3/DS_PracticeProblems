7a669531b577d13daa70100de5d1894db54f2127
dhruv3 <dhruv.mnit@gmail.com>
2017-01-19 21:48:33 -0500

    Adding Binary Tree and solving one problem

---
+++ src/BinaryTree/P1.java
@@ -0,0 +1,42 @@
+package BinaryTree;
+
+public class P1 {
+
+	private static int MAX_Int = Integer.MIN_VALUE;
+
+	public static void main(String[] args) {
+		BinaryTreeNode root = createTree();
+		PreOrderMaxNode(root);
+		System.out.println("Max Integer is " + MAX_Int);
+	}
+
+	private static void PreOrderMaxNode(BinaryTreeNode root) {
+		if(root != null){
+			if(root.getData() > MAX_Int){
+				MAX_Int = root.getData();
+			}
+			PreOrderMaxNode(root.getLeft());
+			PreOrderMaxNode(root.getRight());
+		}
+	}
+	
+	private static BinaryTreeNode createTree() {
+		BinaryTreeNode n1 = new BinaryTreeNode(11);
+		BinaryTreeNode n2 = new BinaryTreeNode(2);
+		BinaryTreeNode n3 = new BinaryTreeNode(3);
+		n1.setLeft(n2);
+		n1.setRight(n3);
+		BinaryTreeNode n4 = new BinaryTreeNode(4);
+		BinaryTreeNode n5 = new BinaryTreeNode(5);
+		n2.setLeft(n4);
+		n2.setRight(n5);
+		BinaryTreeNode n6 = new BinaryTreeNode(6);
+		BinaryTreeNode n7 = new BinaryTreeNode(7);
+		n3.setLeft(n6);
+		n3.setRight(n7);
+		
+		BinaryTreeNode root = n1;
+		
+		return root;
+	}
+}
