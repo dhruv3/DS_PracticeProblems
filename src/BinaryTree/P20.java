65a1a14b1731bd9aef7eac96b5b265b3a316d031
dhruv3 <dhruv.mnit@gmail.com>
2017-03-15 01:35:05 -0400

    P20: Get diameter of a binary tree; Issue is that it assumes that longest path goes through the main root.

---
+++ src/BinaryTree/P20.java
@@ -0,0 +1,55 @@
+package BinaryTree;
+
+public class P20 {
+
+	static int diameter = -1;
+	static BinaryTreeNode mainRoot = null; 
+	
+	public static void main(String[] args) {
+		BinaryTreeNode root = createTree();
+		mainRoot = root;
+		int diameter = diameterOfTree(root);
+		
+		System.out.println("Tree diameter is " + diameter);
+	}
+
+	private static int diameterOfTree(BinaryTreeNode root) {
+		int left, right;
+		if(root == null)
+			return 0;
+		
+		left = diameterOfTree(root.getLeft());
+		right = diameterOfTree(root.getRight());
+		
+		if(left + right >= diameter){
+			diameter = left + right;
+		}
+		//check added 
+		if(root == mainRoot){
+			return diameter + 1;
+		}
+		//modified this part
+		return Math.max(left, right) + 1;
+	}
+
+	private static BinaryTreeNode createTree() {
+		BinaryTreeNode n1 = new BinaryTreeNode(1);
+		BinaryTreeNode n2 = new BinaryTreeNode(2);
+		BinaryTreeNode n3 = new BinaryTreeNode(3);
+		n1.setLeft(n2);
+		n1.setRight(n3);
+		BinaryTreeNode n4 = new BinaryTreeNode(4);
+		BinaryTreeNode n5 = new BinaryTreeNode(5);
+		n2.setLeft(n4);
+		n2.setRight(n5);
+		BinaryTreeNode n6 = new BinaryTreeNode(6);
+		BinaryTreeNode n7 = new BinaryTreeNode(7);
+		n3.setLeft(n6);
+		n3.setRight(n7);
+		
+		BinaryTreeNode root = n1;
+		
+		return root;
+	}
+	
+}
