0ded8904538067180feea2c300ee339bc9645d36
dhruv3 <dhruv.mnit@gmail.com>
2017-03-16 14:25:05 -0400

    P34: zigzag tree traversal P35: vertical sum of BT

---
+++ src/BinaryTree/P34.java
@@ -0,0 +1,76 @@
+package BinaryTree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class P34 {
+
+	public static void main(String[] args) {
+		BinaryTreeNode root = createTree();
+		zigZagTraversal(root);
+	}
+	
+	
+	private static void zigZagTraversal(BinaryTreeNode root) {
+		int dirCtr = 0;
+		Queue<BinaryTreeNode> myQ = new LinkedList<BinaryTreeNode>();
+		Stack<BinaryTreeNode> mySt = new Stack<BinaryTreeNode>();
+		
+		myQ.add(root);
+		
+		while(!myQ.isEmpty()){
+			BinaryTreeNode temp = myQ.poll();
+			System.out.println(temp.getData());
+			//LC then RC
+			if(dirCtr%2 == 0){
+				if(temp.getLeft() != null){
+					mySt.push(temp.getLeft());
+				}
+				if(temp.getRight() != null){
+					mySt.push(temp.getRight());
+				}
+			}
+			//RC then LC
+			else{
+				if(temp.getRight() != null){
+					mySt.push(temp.getRight());
+				}
+				if(temp.getLeft() != null){
+					mySt.push(temp.getLeft());
+				}
+			}
+			//queue is empty(a level is over)
+			//stack not empty(all the children have been added to the stack)
+			if(myQ.isEmpty() && !mySt.isEmpty()){
+				//increment directional counter so as to change direction 
+				dirCtr++;
+				//till stack gets empty add content to queue
+				while(!mySt.isEmpty()){
+					myQ.add(mySt.pop());
+				}
+			}
+		}
+	}
+
+	private static BinaryTreeNode createTree() {
+		BinaryTreeNode n1 = new BinaryTreeNode(1);
+		BinaryTreeNode n2 = new BinaryTreeNode(2);
+		BinaryTreeNode n3 = new BinaryTreeNode(3);
+		n1.setLeft(n2);
+		n1.setRight(n3);
+		BinaryTreeNode n4 = new BinaryTreeNode(4);
+		BinaryTreeNode n5 = new BinaryTreeNode(5);
+		n2.setLeft(n4);
+		n2.setRight(n5);
+		BinaryTreeNode n6 = new BinaryTreeNode(6);
+		BinaryTreeNode n7 = new BinaryTreeNode(7);
+		n3.setLeft(n6);
+		n3.setRight(n7);
+		
+		BinaryTreeNode root = n1;
+		
+		return root;
+	}
+	
+}
---
+++ src/BinaryTree/P35.java
@@ -0,0 +1,59 @@
+package BinaryTree;
+
+import java.util.HashMap;
+
+public class P35 {
+
+	public static void main(String[] args) {
+		BinaryTreeNode root = createTree();
+		verticalSum(root);
+	}
+	
+	
+	private static void verticalSum(BinaryTreeNode root) {
+		HashMap<Integer, Integer> hash = new HashMap<Integer, Integer>();
+		vSum(hash, root, 0);
+		
+		for(int k: hash.keySet()){
+			System.out.println(k + " : " + hash.get(k));
+		}
+	}
+
+
+	private static void vSum(HashMap<Integer, Integer> hash, BinaryTreeNode root, int i) {
+		if(root.getLeft() != null)
+			vSum(hash, root.getLeft(), i - 1);
+		if(root.getRight() != null)
+			vSum(hash, root.getRight(), i + 1);
+		
+		int data = 0;
+		//key is Horizontal distance
+		//value is the value of node
+		if(hash.containsKey(i))
+			data = (int) hash.get(i);
+		
+		hash.put(i, root.getData() + data);
+	}
+
+
+	private static BinaryTreeNode createTree() {
+		BinaryTreeNode n1 = new BinaryTreeNode(1);
+		BinaryTreeNode n2 = new BinaryTreeNode(2);
+		BinaryTreeNode n3 = new BinaryTreeNode(3);
+		n1.setLeft(n2);
+		n1.setRight(n3);
+		BinaryTreeNode n4 = new BinaryTreeNode(4);
+		BinaryTreeNode n5 = new BinaryTreeNode(5);
+		n2.setLeft(n4);
+		n2.setRight(n5);
+		BinaryTreeNode n6 = new BinaryTreeNode(6);
+		BinaryTreeNode n7 = new BinaryTreeNode(7);
+		n3.setLeft(n6);
+		n3.setRight(n7);
+		
+		BinaryTreeNode root = n1;
+		
+		return root;
+	}
+	
+}
