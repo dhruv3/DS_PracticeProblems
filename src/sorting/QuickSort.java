7784d70a0f5003f2bcddf8fc1e06dd0d4646f4ba
dhruv3 <dhruv.mnit@gmail.com>
2017-04-06 23:52:44 -0400

    QuickSort initial commit

---
+++ src/sorting/QuickSort.java
@@ -0,0 +1,19 @@
+package sorting;
+
+public class QuickSort {
+
+	public static void main(String[] args) {
+		int[] inpArr = {7, 4, 5, 2, 8, 3, 6};
+		int[] sortedArr = qSort(inpArr);
+		for(int i = 0 ; i < sortedArr.length; i++){
+			System.out.print(sortedArr[i] + " ");
+		}
+
+	}
+
+	private static int[] qSort(int[] inpArr) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
