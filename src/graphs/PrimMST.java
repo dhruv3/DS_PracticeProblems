3aed5ce3db44b897c688c6407a5519f78e42afb2
dhruv3 <dhruv.mnit@gmail.com>
2017-04-11 18:56:41 -0400

    Minimal changes made. Dijsktra and Prim are same except an additional array is maintained in Prim for MST

--- src/graphs/PrimMST.java
+++ src/graphs/PrimMST.java
@@ -1,19 +1,21 @@
 package graphs;
-//tutorial: http://www.geeksforgeeks.org/greedy-algorithms-set-6-dijkstras-shortest-path-algorithm/
-//implemented without using heaps
+
+//tutorial: http://www.geeksforgeeks.org/greedy-algorithms-set-5-prims-minimum-spanning-tree-mst-2/
 public class PrimMST {
 	
 	public static AdjacencyListG4G g;
 	
 	public static void main(String[] args) {
 		createGraph();
-		System.out.println("Shortest distance calculated from vertex 0");
-		dijkstra(0);
+		System.out.println("MST of the given graph using Prim's Algorithm");
+		prim(0);
 	}
 	
-	private static void dijkstra(int startVertex) {
+	private static void prim(int startVertex) {
 		int totalVertex = g.vertexCount();
-		boolean[] sptSet = new boolean[totalVertex];
+		
+		int[] parent = new int[totalVertex];
+		boolean[] mstSet = new boolean[totalVertex];
 		//array to hold distances
 		int[] dist = new int[totalVertex];
 		//initialize array
@@ -22,33 +24,35 @@ public class PrimMST {
 		}
 		
 		dist[startVertex] = 0;
+		parent[0] = -1;
 		//looping till totalVertex-1
 		for(int idx = 0; idx < totalVertex-1; idx++){
 			//select a vertex
-			int selVtx = minDist(dist, sptSet);
-			sptSet[selVtx] = true;
+			int selVtx = minDist(dist, mstSet);
+			mstSet[selVtx] = true;
 			//update neighboring vertices
 			LinkedList ll = g.getList(selVtx);
 			listNode temp = ll.getHead();
 			while(temp != null){
 				int v = temp.getData();
 				//neighbor not yet visited and distance is larger
-				if(sptSet[v] == false && (dist[v] > dist[selVtx] + temp.getWeight())){
+				if(mstSet[v] == false && (dist[v] > dist[selVtx] + temp.getWeight())){
+					parent[v] = selVtx;
 					dist[v] = dist[selVtx] + temp.getWeight();
 				}
 				temp = temp.getNode();
 			}
 		}
 		//print results
-		printDist(dist);
+		printDist(parent);
 	}
 	
-	//get vertex with min dist value and that vertex is not yet in shortest path set
-	private static int minDist(int[] dist, boolean[] sptSet) {
+	//get vertex with min dist value and that vertex is not yet in minimum set
+	private static int minDist(int[] dist, boolean[] mstSet) {
 		int min = Integer.MAX_VALUE;
 		int v = -1;
-		for(int  i = 0; i < sptSet.length; i++){
-			if(sptSet[i] == false && dist[i] < min){
+		for(int  i = 0; i < mstSet.length; i++){
+			if(mstSet[i] == false && dist[i] < min){
 				min = dist[i];
 				v = i;
 			}
@@ -56,28 +60,20 @@ public class PrimMST {
 		return v;
 	}
 
-	private static void printDist(int[] dist) {
-		System.out.println("Vertex" + "\t" + "Distance");
-		for(int i = 0; i < dist.length; i++){
-			System.out.print("  " + i + "  " + "\t" + "   " +  dist[i] + "  " + "\n");
+	private static void printDist(int[] parent) {
+		for(int i = 1; i < parent.length; i++){
+			System.out.print("  " + i + "  " + "\t" + "   " +  parent[i] + "  " + "\n");
 		}
 	}
 
 	private static void createGraph() {
-		g = new AdjacencyListG4G(9);
-		g.addUndirEdge(0, 1, 4);
-        g.addUndirEdge(0, 7, 8);
-        g.addUndirEdge(1, 2, 8);
-        g.addUndirEdge(1, 7, 11);
-        g.addUndirEdge(2, 3, 7);
-        g.addUndirEdge(2, 8, 2);
-        g.addUndirEdge(2, 5, 4);
+		g = new AdjacencyListG4G(5);
+		g.addUndirEdge(0, 1, 2);
+        g.addUndirEdge(0, 3, 6);
+        g.addUndirEdge(1, 3, 8);
+        g.addUndirEdge(1, 2, 3);
+        g.addUndirEdge(1, 4, 5);
+        g.addUndirEdge(2, 4, 7);
         g.addUndirEdge(3, 4, 9);
-        g.addUndirEdge(3, 5, 14);
-        g.addUndirEdge(4, 5, 10);
-        g.addUndirEdge(5, 6, 2);
-        g.addUndirEdge(6, 8, 6);
-        g.addUndirEdge(6, 7, 1);
-        g.addUndirEdge(7, 8, 7);
 	}
 }
