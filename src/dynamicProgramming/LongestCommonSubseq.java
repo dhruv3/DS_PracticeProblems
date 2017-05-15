6e954b37a0a124771c5b70122b07c90067698d6b
dhruv3 <dhruv.mnit@gmail.com>
2017-05-15 18:58:33 -0400

    Adding DP case for longest subseq

--- src/dynamicProgramming/LongestCommonSubseq.java
+++ src/dynamicProgramming/LongestCommonSubseq.java
@@ -1,12 +1,56 @@
 package dynamicProgramming;
 
+//video tutorial: https://www.youtube.com/watch?v=NnD96abizww
 public class LongestCommonSubseq {
 
 	public static void main(String[] args) {
-		String s1 = "ABCDEF";
-		String s2 = "ACBCF";
+		String s1 = "ACBCF";
+		String s2 = "ABCDAF";
+		
 		String result = LCSRecursive(s1, s2);
-		System.out.println("Longest common subsequence is " + result);
+		System.out.println("Longest common subsequence using recursion is " + result);
+		
+		result = LCSByDP(s1, s2);
+		System.out.println("Longest common subsequence using DP is " + result);
+	}
+
+	private static String LCSByDP(String X, String Y) {
+		int xLen = X.length();
+		int yLen = Y.length();
+		
+		int[][] dpTable = new int[xLen+1][yLen+1];
+		//updating the table
+		for(int i = 1; i <= xLen; i++){
+			for(int j = 1; j <= yLen; j++){
+				if(X.charAt(i-1) == Y.charAt(j-1)){
+					dpTable[i][j] = dpTable[i-1][j-1] + 1;
+				}
+				else{
+					if(dpTable[i-1][j] > dpTable[i][j-1]){
+						dpTable[i][j] = dpTable[i-1][j];
+					}
+					else{
+						dpTable[i][j] = dpTable[i][j-1];
+					}
+				}
+			}
+		}
+		//getting the string out of the table
+		//stringbuffer creates a mutable string
+		StringBuffer result = new StringBuffer();
+		for(int x = X.length(), y = Y.length(); x != 0 && y != 0;){
+			if(dpTable[x][y] == dpTable[x-1][y])
+				x--;
+			else if(dpTable[x][y] == dpTable[x][y-1])
+				y--;
+			else{
+				result.append(X.charAt(x-1));
+				x--;
+				y--;
+			}
+		}
+		
+		return result.reverse().toString();
 	}
 
 	//total of 3 cases
