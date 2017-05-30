306166f41ac5805074815026af7183ace1f8dca7
dhruv3 <dhruv.mnit@gmail.com>
2017-05-29 20:39:40 -0400

    IInitial commit KMP

---
+++ src/string/KMP.java
@@ -0,0 +1,11 @@
+package string;
+
+public class KMP {
+
+	public static void main(String[] args) {
+		String input = "Brown fox jumps over a sleeping something like this is a long sentence";
+		String pattern = "pingi some";
+
+	}
+
+}
