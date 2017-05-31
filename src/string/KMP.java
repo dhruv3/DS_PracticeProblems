7b96db413300f04f9bb91820e3dc82fe7a96c390
dhruv3 <dhruv.mnit@gmail.com>
2017-05-31 16:15:36 -0400

    Prefix table function added

--- src/string/KMP.java
+++ src/string/KMP.java
@@ -2,10 +2,32 @@ package string;
 
 public class KMP {
 
+	public static int[] prefixTable;
+	
 	public static void main(String[] args) {
 		String input = "Brown fox jumps over a sleeping something like this is a long sentence";
 		String pattern = "pingi some";
+		
+		fillPrefixTable(pattern.toCharArray(), pattern.length());
 
+	}
+
+	private static void fillPrefixTable(char[] pattern, int patLength) {
+		int i = 1, j = 0;
+		while(i < patLength){
+			if(pattern[i] == pattern[j]){
+				prefixTable[i] = j+1;
+				i++;
+				j++;
+			}
+			else if(j > 0){
+				j = prefixTable[j - 1];
+			}
+			else{
+				prefixTable[i] = 0;
+				i++;
+			}
+		}
 	}
 
 }
